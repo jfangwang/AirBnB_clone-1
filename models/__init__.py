2551edc1db18bc51a633e14f53a932f3eab0e302
AmilcarArmmand <armmand@gmail.com>
2021-03-30 01:43:44 +0000

    updated to have environmental variable set storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""
+This module instantiates an object of class FileStorage or BDStorage
+"""
+
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from os import environ
 
 
-storage = FileStorage()
+if environ("HBNB_TYPE_STORAGE") == "db":
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
