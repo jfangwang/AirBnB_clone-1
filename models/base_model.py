3c1c98a7a0cecba22b4aa37d334a1e7923d7a377
jfangwang <qbs1864@gmail.com>
2021-03-31 02:55:08 +0000

    added state and city file, save func not working in console

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+# import models.storage import storage
+# from models.amenity import Amenity
+# from models import City
+# from models import Place
+# from models import Review
+# from models import State
+# from models import User
 from datetime import datetime
-
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+
+    """ Declare SQLAlchemy Table"""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -13,14 +27,19 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, val in kwargs.items():
+                """Convert from str to object"""
+                if key == 'updated_at':
+                    val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, val)
+            # kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            #                                          '%Y-%m-%dT%H:%M:%S.%f')
+            # kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            #                                          '%Y-%m-%dT%H:%M:%S.%f')
+            # del kwargs['__class__']
+            # self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -31,7 +50,8 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        storage.new(self)
+        self.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -41,4 +61,9 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary:
+            dictionary.pop('_sa_instance_state')
         return dictionary
+
+    def delete(self):
+        models.storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from models.city import City
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
-class State(BaseModel):
+
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("cities", backref="state", cascade="all, delete")
+
+    def cities(self):
+        """getter attr"""
+        new_list = []
+        for key, val in models.storage.all(City):
+            if key.state_id == self.id:
+                new_list[key] = value
+        return new_list
