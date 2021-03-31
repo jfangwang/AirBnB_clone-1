c86b132601b980615e3f5f2826d7895476419aa9
jfangwang <qbs1864@gmail.com>
2021-03-31 17:28:26 +0000

    Not working

--- models/amenity.py
+++ models/amenity.py
@@ -2,6 +2,7 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from models.base_model import Base
+from models.place import place_amenity, Place
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -10,4 +11,4 @@ class Amenity(BaseModel, Base):
     """Amenity Class"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity")
+    place_amenities = relationship("Place", secondary=place_amenity)
