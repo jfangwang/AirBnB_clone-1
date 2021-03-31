a3fa640a85cb78252ea4174bbd4bedf2d90eff83
AmilcarArmmand <armmand@gmail.com>
2021-03-31 05:25:58 +0000

    added environmental variable classifications

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -32,11 +32,11 @@ class DBStorage:
 
     def __init__(self):
         """Instance of session and db_ojbect """
-        HBNB_MYSQL_USER = os.getenv['HBNB_MYSQL_USER']
-        HBNB_MYSQL_PWD = os.getenv['']
-        HBNB_MYSQL_HOST = os.getenv['']
-        HBNB_MYSQL_DB = os.getenv['']
-        HBNB_ENV = os.getenv['']
+        HBNB_MYSQL_USER = os.getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = os.getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = os.getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = os.getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = os.getenv('HBNB_TYPE_STORAGE')
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                                       format(HBNB_MYSQL_USER,
                                              HBNB_MYSQL_PWD,
