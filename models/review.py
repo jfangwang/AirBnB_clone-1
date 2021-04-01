31c246b94ed6cf7ecc1767d04fe5597f62795bac
AmilcarArmmand <armmand@gmail.com>
2021-04-01 01:31:01 +0000

    Fixed for pep8 check

--- models/review.py
+++ models/review.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 import models
 from sqlalchemy import Column, Integer, String, ForeignKey
 
+
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
