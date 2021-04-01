382e5b5243d7d12fff204edf5727e0c0fce75fee
AmilcarArmmand <armmand@gmail.com>
2021-04-01 01:28:38 +0000

    Fixed for pep8 check

--- models/city.py
+++ models/city.py
@@ -10,7 +10,8 @@ class City(BaseModel, Base):
     try:
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
-        state_id = Column('state_id', String(60), ForeignKey("states.id"), nullable=False)
+        state_id = Column('state_id', String(60),
+                          ForeignKey("states.id"), nullable=False)
         places = relationship('Place', cascade="all, delete", backref="cities")
     except:
         print("city did not work")
