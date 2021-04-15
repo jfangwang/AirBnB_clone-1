08be1e3987bcb590ffa46f45a5b6c3079c19ea27
jfangwang <qbs1864@gmail.com>
2021-04-15 19:53:07 +0000

    2 done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,14 +1,40 @@
 #!/usr/bin/python3
 # distribute archive file
+import os.path
 from fabric.api import local
 from fabric.api import put
 from fabric.api import env
 from fabric.api import run
 from datetime import datetime
 
-env.hosts = [""]
+env.hosts = ["34.75.56.162", "35.237.17.44"]
 
 
 def do_deploy(archive_path):
     """deploy"""
-    if os.path.isfile(archive_path)
+    if not os.path.isfile(archive_path):
+        return False
+    file_name = archive_path.split("/")[-1]
+    name = file_name.split(".")[0]
+
+    if put(archive_path, "tmp/{}".format(file_name)).failed:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}".format(file_name)).failed:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".
+           format(file_name, file_name)).failed:
+        return False
+    if run("rm /tmp/{}".format(file_name)).failed:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* /data/web_static/"
+           "releases/{}/".format(file_name, file_name)).failed:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(file_name)).failed:
+        return False
+    if run("rm -rf /data/web_static/current").failed:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(file_name)).failed:
+        return False
+    return True
