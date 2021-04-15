d6471d31a99d04e0e6633ac884ec6b3411b3b460
jfangwang <qbs1864@gmail.com>
2021-04-15 20:33:15 +0000

    fixed mkdir errors

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,11 +22,11 @@ def do_deploy(archive_path):
     file_name = archive_path.split("/")[-1]
     name = file_name.split(".")[0]
 
-    if put(archive_path, "tmp/{}".format(file_name)).failed:
+    if put(archive_path, "/tmp/{}".format(file_name)).failed:
         return False
-    if run("mkdir -p /data/web_static/releases/{}".format(file_name)).failed:
+    if run("mkdir -p /data/web_static/releases/{}/".format(file_name)).failed:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
            format(file_name, file_name)).failed:
         return False
     if run("rm /tmp/{}".format(file_name)).failed:
