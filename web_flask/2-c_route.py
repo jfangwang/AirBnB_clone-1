7da593d5c918cea3b964f79cabb472fd2930fc1d
jfangwang <qbs1864@gmail.com>
2021-05-01 19:09:28 +0000

    forgot the _ rule

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,6 +19,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """hello text"""
+    text = text.replace("_", " ")
     return "C " + text
 
 if __name__ == '__main__':
