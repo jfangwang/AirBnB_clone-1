fb24f90eb4b9f4ad6a0dc9d81bc1809b724a51cc
jfangwang <qbs1864@gmail.com>
2021-05-02 21:39:34 +0000

    8 fixing

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -17,7 +17,8 @@ app = Flask(__name__)
 def state_list():
     """hello even or odd number"""
     states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+    cities = storage.all("City")
+    return render_template("8-cities_by_states.html", states=states, cities=cities)
 
 
 @app.teardown_appcontext
