15fac934aca3b89a18f73421d3b34e842229d17c
jfangwang <qbs1864@gmail.com>
2021-05-02 20:22:54 +0000

    8 still working on it, just want ot submit to see what's up

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -65,6 +65,13 @@ def state_list():
     return render_template("7-states_list.html", states=states)
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_list():
+    """Cities by states"""
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
+
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """teardown"""
